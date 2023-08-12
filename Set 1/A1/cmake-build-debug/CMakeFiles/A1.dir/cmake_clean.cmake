949b450f2eccaab99fb1cd946db2d2fd400f8130
Lauren Sherburne <98053027+sherbzy@users.noreply.github.com>
2023-08-12 01:03:33 -0600

    Upload A1 project files

---
+++ Set 1/A1/cmake-build-debug/CMakeFiles/A1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/A1.dir/main.cpp.obj"
+  "A1.pdb"
+  "A1.exe"
+  "A1.exe.manifest"
+  "libA1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/A1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
