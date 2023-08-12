4605653adbd85436a4a1a05d5e7a4ee6b9f15a5c
Lauren Sherburne <98053027+sherbzy@users.noreply.github.com>
2023-08-12 01:04:26 -0600

    Upload L1A lab files

---
+++ Set 1/L1A/cmake-build-debug/CMakeFiles/L1A.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/L1A.dir/main.cpp.obj"
+  "L1A.pdb"
+  "L1A.exe"
+  "L1A.exe.manifest"
+  "libL1A.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/L1A.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Set 1/L1A/main.cpp
@@ -0,0 +1,28 @@
+/* CSCI 261 Lab 1A: XXXX (_GIVE_BRIEF_DESCRIPTION_HERE_)
+ *
+ * Author: Lauren Sherburne
+ *
+ * More complete description here...
+ */
+
+// The include section adds extra definitions from the C++ standard library.
+#include <iostream> // For cin, cout, etc.
+
+// We will (most of the time) use the standard library namespace in our programs.
+using namespace std;
+
+// Define any constants or global variables below this comment.
+
+// Must have a function named "main", which is the starting point of a C++ program.
+int main() {
+
+    /******** INSERT YOUR CODE BELOW HERE ********/
+
+    cout << "Hello world!" << endl; // print Hello world! to the screen
+    cout << "How are you?" << endl; // print How are you? to the screen
+    cout << "(I'm fine)" << endl; //print (I'm fine) to the screen
+
+    /******** INSERT YOUR CODE ABOVE HERE ********/
+
+    return 0; // signals the operating system that our program ended OK.
+}
