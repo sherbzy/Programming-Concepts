66b698eef3a5297b20295735743d2f9f78669642
Lauren Sherburne <98053027+sherbzy@users.noreply.github.com>
2023-08-12 00:02:54 -0700

    Create "Set 1" folder

---
+++ Set 1/A1/main.cpp
@@ -0,0 +1,34 @@
+/*
+ * Program Name: Smiley Face Drawing and 3 Facts About Me
+ * Author: Lauren Sherburne
+ * Date: August 23, 2019
+ */
+//The code below identifies the library and namespace to be used in this program.
+#include <iostream>
+using namespace std;
+
+//The following instructs the computer to draw a smiley face giving a thumbs up.
+//Then, it displays "Hello World!", lists 3 facts about me, and displays "Goodbye."
+int main() {
+    cout << "´´´´´´´´´´´´´´´´´´´´´´´¶¶¶¶¶¶¶¶" << endl;
+    cout << "´´´´´´¶¶¶¶¶´´´´´´´´¶¶¶¶´´´´´´´´¶¶¶¶" << endl;
+    cout << "´´´´´¶´´´´´¶´´´´¶¶´´´´´´´´´´´´´´´´´´¶¶" << endl;
+    cout << "´´´´´¶´´´´´¶´´´¶¶´´´´´´¶¶´´´´¶¶´´´´´´´¶¶" << endl;
+    cout << "´´´´´¶´´´´¶´´¶¶´´´´´´´´¶¶´´´´¶¶´´´´´´´´¶¶" << endl;
+    cout << "´´´´´´¶´´´¶´´´¶´´´´´´´´´´´´´´´´´´´´´´´´´¶¶" << endl;
+    cout << "´´´´¶¶¶¶¶¶¶¶¶¶¶¶´´´´´´´´´´´´´´´´´´´´´´´´´¶¶" << endl;
+    cout << "´´´¶´´´´´´´´´´´´¶´´´¶¶´´´´´´´´´´´¶¶´´´´´´¶¶" << endl;
+    cout << "´´¶¶´´´´´´´´´´´´¶´´´¶¶´´´´´´´´´´´¶¶´´´´´´¶¶" << endl;
+    cout << "´¶¶´´´¶¶¶¶¶¶¶¶¶¶¶´´´´´¶¶´´´´´´´¶¶´´´´´´´¶¶" << endl;
+    cout << "'¶´´´´´´´´´´´´´´´¶´´´´´´¶¶¶¶¶¶¶´´´´´´´´¶¶" << endl;
+    cout << "´¶¶´´´´´´´´´´´´´´¶´´´´´´´´´´´´´´´´´´´´¶¶" << endl;
+    cout << "´´¶´´´¶¶¶¶¶¶¶¶¶¶¶¶´´´´´´´´´´´´´´´´´´´¶¶" << endl;
+    cout << "´´¶¶´´´´´´´´´´´¶´´´¶¶¶¶´´´´´´´´´¶¶¶¶" << endl;
+    cout << "´´´¶¶¶¶¶¶¶¶¶¶¶¶´´´´´´´´¶¶¶¶¶¶¶¶¶" << endl;
+    cout << "Hello World!" << endl;
+    cout << "1. I grew up in San Diego, CA." << endl;
+    cout << "2. I have 2 sisters: Anna and Lucy" << endl;
+    cout << "3. This April, I had knee surgery to repair my acl and meniscus." << endl;
+    cout << "Goodbye." << endl;
+    return 0;
+}
