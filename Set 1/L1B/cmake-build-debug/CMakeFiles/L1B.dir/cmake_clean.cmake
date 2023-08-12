b7194bfad1b67ce4e8a63c8d691c22f4e87078fe
Lauren Sherburne <98053027+sherbzy@users.noreply.github.com>
2023-08-12 01:05:13 -0600

    Upload L1B lab files

---
+++ Set 1/L1B/cmake-build-debug/CMakeFiles/L1B.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/L1B.dir/main.cpp.obj"
+  "L1B.pdb"
+  "L1B.exe"
+  "L1B.exe.manifest"
+  "libL1B.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/L1B.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Set 1/L1B/main.cpp
@@ -0,0 +1,22 @@
+/* CSCI 261 Lab 1B: ASCII ART INTRO
+ * Program Name: Christmas Tree and Cat Drawing(L1B)
+ * Author: Lauren Sherburne
+ * Date: August 21, 2019
+ */
+
+#include <iostream>
+
+int main() {
+    std::cout << "   *" << std::endl;
+    std::cout << "  ***" << std::endl;
+    std::cout << " *****" << std::endl;
+    std::cout << "*******" << std::endl;
+    std::cout << "  ***" << std::endl;
+    std::cout << std::endl;
+    std::cout<< std::endl;
+    std::cout << "/\\   /\\" << std::endl;
+    std::cout << "  o o  " << std::endl;
+    std::cout << " =   = " << std::endl;
+    std::cout << "  ---  " << std::endl;
+    return 0;
+}
